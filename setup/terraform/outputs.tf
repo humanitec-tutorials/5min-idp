475dbb1d767e0a87c4bf5a91c7800947b3a2ad54
mouhsen.ibrahim <mouhsen.ibrahim@gmail.com>
2025-03-02 22:02:32 +0100

    Make sure that humanitec environment is created

--- setup/terraform/outputs.tf
+++ setup/terraform/outputs.tf
@@ -2,3 +2,8 @@ output "humanitec_app" {
   description = "The ID of the Humanitec application"
   value       = humanitec_application.demo.id
 }
+
+output "humanitec_environment" {
+  description = "The ID of the Humanitec environment"
+  value       = humanitec_environment.demo.id
+}
